adba08b13e422add18e1ad64ca2816d79cb4d239
ToniHeinilä <toni.heinila@skillio.ai>
2024-10-31 09:47:47 +0200

    Project structure

---
+++ src/data/config.py
@@ -0,0 +1,15 @@
+from configparser import ConfigParser
+def config(filename="database.ini", section="postgresql"):
+    parser = ConfigParser()
+    parser.read(filename)
+    db = {}
+
+    if parser.has_section(section):
+        params = parser.items(section)
+        for p in params:
+            db[p[0]] = p[1]
+    else:
+        raise Exception(f"Section {section} not found in file {filename}")
+    
+    return db
+
---
+++ src/data/queries.py
@@ -0,0 +1,8 @@
+import psycopg2
+import sys
+from config import config
+
+def main():
+    pass
+
+main()
